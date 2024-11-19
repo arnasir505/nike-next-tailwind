a0244098fec418ed8d550e8046d492a7e003699f
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-11-19 16:18:44 +0600

    Create Special Offer  section

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,8 +1,26 @@
 import Image from 'next/image';
 
-const Button = ({ label, iconUrl }: { label: string; iconUrl?: string }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}: {
+  label: string;
+  iconUrl?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
+}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : `bg-coral-red text-white border-coral-red`
+      }`}
+    >
       {label}
       {iconUrl && (
         <Image
